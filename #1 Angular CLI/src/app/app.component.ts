9ccd302a445a93713ddf936a820da6c950844731
Slate & Chalk <58339738+slateandchalk@users.noreply.github.com>
2020-02-08 12:57:55 +0530

    #1 Angular CLI

---
+++ #1 Angular CLI/src/app/app.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+} from '@angular/animations';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('openClose', [
+      state('open', style({
+        height: '200px',
+        opacity: 1,
+        backgroundColor: 'yellow'
+      })),
+      state('closed', style({
+        height: '100px',
+        opacity: 0.5,
+        backgroundColor: 'green'
+      })),
+      transition('open => closed', [
+        animate('1s')
+      ]),
+      transition('closed => open', [
+        animate('0.5s')
+      ]),
+    ]),
+  ]
+})
+export class AppComponent {
+  title = 'First';
+
+  isOpen = true;
+
+  toggle(){
+    console.log('j')
+    this.isOpen = !this.isOpen;
+  }
+}
